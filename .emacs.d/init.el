3151a9a379d0fc58f751dbefb548051e3efe1a7d
Lungang Fang <6370184+lgfang@users.noreply.github.com>
2023-09-07 15:30:29 +1000

    emacs: init.el package-install git-gutter & add comment on its usage

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,7 @@
 ;;; lgfang.init.el --- my configuration file
 
 ;; Created:  Lungang Fang 2004
-;; Modified: Lungang Fang 2023-09-06T10:04:27+1000>
+;; Modified: Lungang Fang 2023-09-07T15:29:45+1000>
 
 ;;; Commentary:
 
@@ -747,6 +747,30 @@ tmux's buffer"
 
 ;;; gdb
 ;; (setq gdb-many-windows t)
+
+;;; git-gutter
+;; (when (require 'git-gutter)
+
+;;   ;; To diff with a revision other than the latest one, in the repo root
+;;   ;; directory, add to the emacs directory local variable file (.dir-locals.el)
+;;   ;; add content similar to the following:
+;;   ;;
+;;   ;; ((prog-mode . ((git-gutter:start-revision . "my_branch"))))
+
+;;   (global-git-gutter-mode t)
+
+;;   ;; Jump to next/previous hunk
+;;   (global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+;;   (global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+
+;;   ;; Stage current hunk
+;;   (global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+
+;;   ;; Revert current hunk
+;;   (global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
+
+;;   (global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+;;   )
 
 ;;; git-link
 (setq git-link-open-in-browser t)
@@ -1688,7 +1712,7 @@ string,refer to format-time-string."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(mermaid-mode protobuf-mode cmake-mode magit anaconda-mode eglot blacken git-link csv-mode emms json-reformat windata w3m solarized-theme showtip terraform-mode highlight-parentheses highlight-indentation org-contrib yasnippet-snippets hide-lines ox-gfm yasnippet pydoc-info pydoc markdown-mode jira-markup-mode ht go-mode flycheck f)))
+   '(git-gutter mermaid-mode protobuf-mode cmake-mode magit anaconda-mode eglot blacken git-link csv-mode emms json-reformat windata w3m solarized-theme showtip terraform-mode highlight-parentheses highlight-indentation org-contrib yasnippet-snippets hide-lines ox-gfm yasnippet pydoc-info pydoc markdown-mode jira-markup-mode ht go-mode flycheck f)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
